b905babeea81de384b079e7f239eda791fff009c
ottobbk <poopo7096@gmail.com>
2025-12-06 15:40:19 +0700

    kk

---
+++ data.js
@@ -0,0 +1,8 @@
+export const data = 
+  {
+    name: "Natthamethee",
+    age: "17",
+    favourite_food: ["suthi", "burger", "noodle"],
+    Hobby: ["playgame", "basketball", "singing"],
+    school: "Watsuthiwararam School"
+  }
--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-/* 
-function handle_mouse_over(element,text){
-    element.InnerHtml = text
-    console.log(text)
-}
-const FB = document.getElementById("FB")
-FB.addEventListener('click',()=>handle_mouse_over(FB,"FB"))
+import { data } from "./data.js"
 
- */
+let con_a = document.createElement("div")
+con_a.setAttribute("class", "container")
+const container = document.getElementById("container")
+
+let h1_e =document.createElement('h1')
+h1_e.innerHTML="KUI"+data.name
+
+container.appendChild(h1_e)
