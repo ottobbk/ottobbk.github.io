ca0d765ebed610eebaca9ad0fe43068147a93db2
Nagraw_1 <poopo7096@gmail.com>
2025-12-06 13:39:56 +0700

    kuy

---
+++ script.js
@@ -0,0 +1,9 @@
+/* 
+function handle_mouse_over(element,text){
+    element.InnerHtml = text
+    console.log(text)
+}
+const FB = document.getElementById("FB")
+FB.addEventListener('click',()=>handle_mouse_over(FB,"FB"))
+
+ */
